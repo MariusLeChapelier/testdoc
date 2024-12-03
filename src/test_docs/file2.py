5b487a02ffb79100bac81af5dd5a66d88de97a19
MariusLeChapelier <mariuslechapelier@gmail.com>
2024-12-03 14:50:47 +0100

    add file2

---
+++ src/test_docs/file2.py
@@ -0,0 +1,17 @@
+"""
+FILE2 HEADER
+============
+
+lala
+"""
+
+import retico_core
+
+
+class File2(retico_core.AbstractModule):
+    """FILE2 CLASS DOCSTRING"""
+
+    def f1(
+        self,
+    ):
+        """FILE2 FUNCTION DOCSTRING"""
